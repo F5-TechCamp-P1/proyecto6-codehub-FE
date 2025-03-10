05d1c1f1db89208c0be2c1fabeb871cd598ee2f0
AlejandroAriasSL <alejandroasilva02@gmail>
2025-03-10 15:38:01 +0100

    add Header.jsx an Header.css and images

---
+++ src/Header.jsx
@@ -0,0 +1,21 @@
+import "./Header.css";
+
+export const Header = () => {
+  return (
+    <header className="bg-dark">
+      <nav className="nav">
+        <div>
+          <img src="/logo.png" alt="Logo" />
+        </div>
+        <p>HTML</p>
+        <p>CSS</p>
+        <p>JavaScript</p>
+        <p>React</p>
+        <p>Java</p>
+        <div>
+          <img src="/user.png" alt="user profile" />
+        </div>
+      </nav>
+    </header>
+  );
+};
