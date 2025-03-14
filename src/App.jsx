1af448f78f26c2ccab18c9a835b698d6190e233d
MabelRincon <mabel.rincon@outlook.com>
2025-03-14 16:03:24 +0100

    Add page, jsx and css. Install react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import React from "react";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./LandingPage";
+import Page from "./page/Page";
 import "./App.css";
 
 function App() {
-  return <LandingPage />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/page" element={<Page />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/page/Page.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "../components/header/Header";
+import Footer from "../components/footer/Footer";
+import Category from "../components/category/Category";
+import "./Page.css";
+
+const Page = () => {
+    return (
+        <div className="page-container">
+            <Header />
+            <main className="content">
+                <Category />
+            </main>
+            <Footer />
+        </div>
+    );
+};
