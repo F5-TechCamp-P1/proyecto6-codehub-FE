186760e2499a7488d8dc6505bb2e39b42017a54b
AlejandroAriasSL <alejandroasilva02@gmail>
2025-03-14 12:57:37 +0100

    feat: add visibility state, refactor buttons into a single component and fix minor styles and resources mistakes

---
+++ src/components/button/Button.jsx
@@ -0,0 +1,10 @@
+import "./Button.css"
+
+export const Button = ({action, handleOnClick}) => {
+
+    return (
+        <button className="action-btn" onClick={() => handleOnClick()}>
+            <img className="categoryImg" src={`/${action}button.png`} alt="delete button" />
+        </button>
+    )
+}
