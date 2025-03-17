63d4a9ed03608727cfae46294e1f7bed51ce500e
AlejandroAriasSL <alejandroasilva02@gmail>
2025-03-17 05:41:43 +0100

    fix styles and naming issues

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,15 +5,17 @@ export const Header = () => {
     <header className="bg-dark">
       <nav className="nav">
         <div>
-          <img src="/logo.png" alt="Logo" />
+          <img className="icon" src="/logo.png" alt="Logo" />
         </div>
-        <p>HTML</p>
-        <p>CSS</p>
-        <p>JavaScript</p>
-        <p>React</p>
-        <p>Java</p>
+        <div className="nav-links">
+          <p>HTML</p>
+          <p>CSS</p>
+          <p>JavaScript</p>
+          <p>React</p>
+          <p>Java</p>
+        </div>
         <div>
-          <img src="/user.png" alt="user profile" />
+          <img className="icon" src="/user.png" alt="user profile" />
         </div>
       </nav>
     </header>
